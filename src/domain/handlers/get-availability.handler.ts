68fbf1af67158acf927ba28b2f8a4f9ec882237b
rolando22 <rolando2207@gmail.com>
2025-01-27 01:37:56 -0300

    Challenge ATC - Optimización

--- src/domain/handlers/get-availability.handler.ts
+++ src/domain/handlers/get-availability.handler.ts
@@ -1,5 +1,6 @@
-import { Inject } from '@nestjs/common';
+import { CACHE_MANAGER, Inject } from '@nestjs/common';
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { Cache } from 'cache-manager';
 
 import {
   ClubWithAvailability,
@@ -17,30 +18,47 @@ export class GetAvailabilityHandler
   constructor(
     @Inject(ALQUILA_TU_CANCHA_CLIENT)
     private alquilaTuCanchaClient: AlquilaTuCanchaClient,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
   async execute(query: GetAvailabilityQuery): Promise<ClubWithAvailability[]> {
-    const clubs_with_availability: ClubWithAvailability[] = [];
+    const key = `clubs-with-availability-${query.placeId}-${query.date}`;
+    const cache_clubs = await this.cacheManager.get<ClubWithAvailability[]>(
+      key,
+    );
+
+    if (cache_clubs) return cache_clubs;
+
     const clubs = await this.alquilaTuCanchaClient.getClubs(query.placeId);
-    for (const club of clubs) {
-      const courts = await this.alquilaTuCanchaClient.getCourts(club.id);
-      const courts_with_availability: ClubWithAvailability['courts'] = [];
-      for (const court of courts) {
-        const slots = await this.alquilaTuCanchaClient.getAvailableSlots(
-          club.id,
-          court.id,
-          query.date,
+
+    const clubs_with_availability: ClubWithAvailability[] = await Promise.all(
+      clubs.map(async (club) => {
+        const courts = await this.alquilaTuCanchaClient.getCourts(club.id);
+
+        const courts_with_availability = await Promise.all(
+          courts.map(async (court) => {
+            const slots = await this.alquilaTuCanchaClient.getAvailableSlots(
+              club.id,
+              court.id,
+              query.date,
+            );
+
+            return {
+              ...court,
+              available: slots,
+            };
+          }),
         );
-        courts_with_availability.push({
-          ...court,
-          available: slots,
-        });
-      }
-      clubs_with_availability.push({
-        ...club,
-        courts: courts_with_availability,
-      });
-    }
+
+        return {
+          ...club,
+          courts: courts_with_availability,
+        };
+      }),
+    );
+
+    await this.cacheManager.set(key, clubs_with_availability, 1000 * 10);
+
     return clubs_with_availability;
   }
 }
--- src/infrastructure/clients/http-alquila-tu-cancha.client.ts
+++ src/infrastructure/clients/http-alquila-tu-cancha.client.ts
@@ -1,6 +1,7 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Cache } from 'cache-manager';
 import * as moment from 'moment';
 
 import { Club } from '../../domain/model/club';
@@ -11,37 +12,83 @@ import { AlquilaTuCanchaClient } from '../../domain/ports/aquila-tu-cancha.clien
 @Injectable()
 export class HTTPAlquilaTuCanchaClient implements AlquilaTuCanchaClient {
   private base_url: string;
-  constructor(private httpService: HttpService, config: ConfigService) {
+  constructor(
+    private httpService: HttpService,
+    config: ConfigService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {
     this.base_url = config.get<string>('ATC_BASE_URL', 'http://localhost:4000');
   }
 
   async getClubs(placeId: string): Promise<Club[]> {
-    return this.httpService.axiosRef
-      .get('clubs', {
-        baseURL: this.base_url,
-        params: { placeId },
-      })
-      .then((res) => res.data);
+    const key = `placeId-${placeId}-clubs`;
+
+    try {
+      const cache_clubs = await this.cacheManager.get<Club[]>(key);
+
+      if (cache_clubs) return cache_clubs;
+
+      const clubs = await this.httpService.axiosRef
+        .get('clubs', {
+          baseURL: this.base_url,
+          params: { placeId },
+        })
+        .then((res) => res.data);
+
+      await this.cacheManager.set(key, clubs, 1000 * 60 * 60);
+
+      return clubs;
+    } catch (error) {
+      return [];
+    }
   }
 
-  getCourts(clubId: number): Promise<Court[]> {
-    return this.httpService.axiosRef
-      .get(`/clubs/${clubId}/courts`, {
-        baseURL: this.base_url,
-      })
-      .then((res) => res.data);
+  async getCourts(clubId: number): Promise<Court[]> {
+    const key = `clubId-${clubId}-courts`;
+
+    try {
+      const cache_courts = await this.cacheManager.get<Court[]>(key);
+
+      if (cache_courts) return cache_courts;
+
+      const courts = await this.httpService.axiosRef
+        .get(`/clubs/${clubId}/courts`, {
+          baseURL: this.base_url,
+        })
+        .then((res) => res.data);
+
+      await this.cacheManager.set(key, courts, 1000 * 60 * 60);
+
+      return courts;
+    } catch (error) {
+      return [];
+    }
   }
 
-  getAvailableSlots(
+  async getAvailableSlots(
     clubId: number,
     courtId: number,
     date: Date,
   ): Promise<Slot[]> {
-    return this.httpService.axiosRef
-      .get(`/clubs/${clubId}/courts/${courtId}/slots`, {
-        baseURL: this.base_url,
-        params: { date: moment(date).format('YYYY-MM-DD') },
-      })
-      .then((res) => res.data);
+    const key = `clubId-${clubId}-courts-${courtId}-date-${date}-slots`;
+
+    try {
+      const cache_slots = await this.cacheManager.get<Slot[]>(key);
+
+      if (cache_slots) return cache_slots;
+
+      const slots = await this.httpService.axiosRef
+        .get(`/clubs/${clubId}/courts/${courtId}/slots`, {
+          baseURL: this.base_url,
+          params: { date: moment(date).format('YYYY-MM-DD') },
+        })
+        .then((res) => res.data);
+
+      await this.cacheManager.set(key, slots, 1000 * 5);
+
+      return slots;
+    } catch (error) {
+      return [];
+    }
   }
 }
